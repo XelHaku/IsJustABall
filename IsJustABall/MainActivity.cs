b898c6c32df61e5f519c6e1d9cb5d0b4de509653
XelHaku <tamez.jm@gmail.com>
2015-01-13 17:52:19 -0600

    Jan 13 First Upload
    Is Just a Ball!

---
+++ IsJustABall/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using CocosSharp;
+using Android.Content.PM;
+using Microsoft.Xna.Framework;
+
+
+namespace test
+{
+	[Activity(
+		Label = "BouncingGame",
+		AlwaysRetainTaskState = true,
+		Icon = "@drawable/icon",
+		Theme = "@android:style/Theme.NoTitleBar",
+		LaunchMode = LaunchMode.SingleInstance,
+		MainLauncher = true,
+		ScreenOrientation = ScreenOrientation.Portrait,
+		ConfigurationChanges =  ConfigChanges.Keyboard | 
+		ConfigChanges.KeyboardHidden)
+	]
+	public class MainActivity : AndroidGameActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			var application = new CCApplication();
+
+			// GameAppDelegate is your class that inherits 
+			// from CCApplicationDelegate
+			application.ApplicationDelegate = new GameAppDelegate();
+			SetContentView(application.AndroidContentView);
+			application.StartGame();
+		}
+	}
+}
+
+
---
+++ IsJustABall/PCL/GameAppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CocosSharp;
+
+
+namespace test
+{
+	public class GameAppDelegate : CCApplicationDelegate
+	{
+		public override void ApplicationDidFinishLaunching (CCApplication application, CCWindow mainWindow)
+		{
+			application.PreferMultiSampling = false;
+			application.ContentRootDirectory = "Content";
+
+			var bounds = mainWindow.WindowSizeInPixels;
+			CCScene.SetDefaultDesignResolution(bounds.Width, bounds.Height, CCSceneResolutionPolicy.ShowAll);
+
+			GameScene gameScene = new GameScene (mainWindow);
+			mainWindow.RunWithScene (gameScene);
+
+		}
+
+		public override void ApplicationDidEnterBackground (CCApplication application)
+		{
+		}
+
+		public override void ApplicationWillEnterForeground (CCApplication application)
+		{
+		}
+	}
+}
+
