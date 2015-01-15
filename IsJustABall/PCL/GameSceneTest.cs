504a1f500846aed7b00ff1d5abd902be53a930a1
XelHaku <tamez.jm@gmail.com>
2015-01-15 17:59:23 -0600

    Jan 15 Progress
    Implement HookedParticle method, almost finish

--- IsJustABall/PCL/GameSceneTest.cs
+++ IsJustABall/PCL/GameSceneTest.cs
@@ -9,17 +9,35 @@ namespace IsJustABall
 
 
 		CCSprite ballSprite;
+		CCSprite pivotSprite;
 		CCLabelTtf scoreLabel;
 
-		float ballXVelocity = 600 ;
-		float ballYVelocity = 600;
+		float ballXVelocity =500 ;
+		float ballYVelocity = 500;
 		// How much to modify the ball's y velocity per second:
 		float gravity = 140;
 		//hookTouchBool=!hookTouchBool// to toggle on Touch
 		bool hookTouchBool = true;
-
-
 		int score;
+		//Declare variables for HookedParticle Method
+		double dRadius_X;
+		double dRadius_Y;
+		double Radius;
+		double ballSpeed;
+		double SinThetaZero;
+		double CosThetaZero;
+		double CosAlpha;
+		double SinAlpha;
+		double theta=0;
+		double ThetaZero=0;
+		double Multiplier =1.1f;
+		double wZero;
+		double ballSpeedFinal;
+		bool ClockwiseRotation = true;
+
+		
+		/// //////////////////
+
 
 
 		CCEventListenerTouchAllAtOnce touchListener;
@@ -32,6 +50,7 @@ namespace IsJustABall
 			var bounds = mainWindow.WindowSizeInPixels;
 
 			addBlueBall(mainWindow);
+			addPivot(mainWindow);
 
 
 			scoreLabel = new CCLabelTtf ("Score: 0", "arial", 22);
@@ -69,7 +88,60 @@ namespace IsJustABall
 		}
 		void HandleTouchesBegan(System.Collections.Generic.List<CCTouch> touches, CCEvent touchEvent){
 			hookTouchBool = !hookTouchBool;//Toggle hook
+			if (hookTouchBool == false) {
+			//Set values for spped and radius or rotation raound Pivot
 
+				dRadius_X = ballSprite.PositionX- pivotSprite.PositionX;
+				dRadius_Y = ballSprite.PositionY- pivotSprite.PositionY;
+
+				Radius = Math.Pow((double)dRadius_X,2) + Math.Pow((double)dRadius_Y,2);
+				Radius = Math.Pow(Radius,0.5);
+
+				ballSpeed = Math.Pow(ballXVelocity,2) + Math.Pow(ballYVelocity,2);
+				ballSpeed = Math.Pow(ballSpeed,0.5d);
+
+				CosThetaZero = dRadius_X/Radius;
+				SinThetaZero = dRadius_Y/Radius;
+
+				CosAlpha =(dRadius_X*ballXVelocity + dRadius_Y*ballYVelocity)/(Radius*ballSpeed);
+
+				if( Math.Pow(CosAlpha,2) < 0.05){
+					CosAlpha = Math.Pow(0.05,0.5);
+				}
+				SinAlpha = (1 - Math.Pow(CosAlpha,2));
+				SinAlpha = Math.Pow(SinAlpha,0.5f);
+
+
+				wZero = ballSpeed/Radius;
+				ballSpeedFinal = Radius*Multiplier*wZero*SinAlpha;
+
+				ThetaZero = Math.Acos (CosThetaZero);
+
+				//Second Quadrant
+				if(SinThetaZero > 0 && CosThetaZero <0){
+					ThetaZero = (1/2)*Math.PI + ThetaZero;
+				}
+				//Third Quadrant
+				if(SinThetaZero <0 && CosThetaZero < 0 ){
+					ThetaZero = (2)*Math.PI - ThetaZero;
+				}
+
+
+
+				//Fourth Quadrant
+				if(SinThetaZero <0 && CosThetaZero > 0 ){
+					ThetaZero =  - ThetaZero;
+				}
+				theta=ThetaZero;
+				//Define if it is clockwise or anticlockwise rotation
+
+				if ((CosThetaZero <= ballXVelocity / ballSpeed && SinThetaZero > 0) || (CosThetaZero >= ballXVelocity / ballSpeed && SinThetaZero < 0)) {
+					ClockwiseRotation = true;
+				} else {
+					ClockwiseRotation = false;
+				}
+			
+			}
 		}
 
 
@@ -80,15 +152,9 @@ namespace IsJustABall
 			ballYVelocity += frameTimeInSeconds * -gravity;
 			ballSprite.PositionX += ballXVelocity * frameTimeInSeconds;
 			ballSprite.PositionY += ballYVelocity * frameTimeInSeconds;
-
-
-
 			// New code:
 			//score++;
 			//scoreLabel.Text = "Score: " + score;
-
-
-
 			// Check if the ball is either too far to the right or left:
 			float ballRight = ballSprite.BoundingBoxTransformedToParent.MaxX;
 			float ballLeft = ballSprite.BoundingBoxTransformedToParent.MinX;
@@ -124,54 +190,29 @@ namespace IsJustABall
 		}
 
 		void hookedParticle(float frameTimeInSeconds){
-			gravity = -140;
+			gravity = 0;
 
-			//delete
-			// This is a linear approximation, so not 100% accurate
-			ballXVelocity += frameTimeInSeconds * -gravity;
-			ballSprite.PositionX += ballXVelocity * frameTimeInSeconds;
-			ballSprite.PositionY += ballYVelocity * frameTimeInSeconds;
+			// below is the iteration over time
+			theta+= (double)(Multiplier*wZero*SinAlpha*frameTimeInSeconds);
 
+			//ballSprite.PositionX =(float)(Radius*Math.Cos(theta) + pivotSprite.PositionX);
+			//ballSprite.PositionY = (float)(Radius*Math.Sin(theta) + pivotSprite.PositionY);
+			//ConsolePrint
+//			Console.WriteLine( "\n Math.Sin({0} deg) == {1:E16}\n" + "Math.Cos({0} deg) == {2:E16}", Math.Sin(theta), Math.Cos(theta) );
+			ballXVelocity = (float)(-ballSpeedFinal * Math.Sin (theta));
+			ballYVelocity = (float)(ballSpeedFinal * Math.Cos (theta));
 
-
-			// New code:
-			//score++;
-			//scoreLabel.Text = "Score: " + score;
-
-
-
-			// Check if the ball is either too far to the right or left:
-			float ballRight = ballSprite.BoundingBoxTransformedToParent.MaxX;
-			float ballLeft = ballSprite.BoundingBoxTransformedToParent.MinX;
-
-			float screenRight = mainLayer.VisibleBoundsWorldspace.MaxX;
-			float screenLeft = mainLayer.VisibleBoundsWorldspace.MinX;
-
-			bool shouldReflectXVelocity = (ballRight > screenRight && ballXVelocity > 0) ||	(ballLeft < screenLeft && ballXVelocity < 0);
+			if (ClockwiseRotation == true) {
+				ballSprite.PositionX =(float)(Radius*Math.Cos(theta) + pivotSprite.PositionX);
+				ballSprite.PositionY = (float)(Radius*Math.Sin(theta) + pivotSprite.PositionY);
 
 
-			if (shouldReflectXVelocity)
-			{
-				ballXVelocity *= -1;
+			} else {
+				ballSprite.PositionX =(float)(-Radius*Math.Cos(theta) + pivotSprite.PositionX);
+				ballSprite.PositionY = (float)(-Radius*Math.Sin(theta) + pivotSprite.PositionY);
 
-			}
-
-			// Check if the ball is either too far to the right or left:
-
-			float ballTop = ballSprite.BoundingBoxTransformedToParent.MaxY;
-			float ballBottom = ballSprite.BoundingBoxTransformedToParent.MinY;
-
-			float screenTop = mainLayer.VisibleBoundsWorldspace.MaxY;
-			float screenBottom = mainLayer.VisibleBoundsWorldspace.MinY;
-
-			bool shouldReflectYVelocity = (ballTop > screenTop && ballYVelocity > 0) ||	(ballBottom < screenBottom && ballYVelocity < 0);
-
-			if (shouldReflectYVelocity)
-			{
-				ballYVelocity *= -1;
-			}
-
-			/////////
+						}
+				/////////
 		}
 
 
@@ -181,27 +222,27 @@ namespace IsJustABall
 			var bounds = mainWindow.WindowSizeInPixels;
 
 			ballSprite = new CCSprite ("blueball");
-			ballSprite.Scale = bounds.Width/5000;
-			ballSprite.PositionX = bounds.Width/5000;
-			ballSprite.PositionY = bounds.Height/5000;
+			ballSprite.Scale = 0.0002f*bounds.Width;
+			ballSprite.PositionX = 0.05f*bounds.Width;
+			ballSprite.PositionY = 0.05f*bounds.Height;
 			mainLayer.AddChild (ballSprite);
 
 
 		}
 
-		/*
-	        void addPivot(CCWindow mainWindow){
+		void addPivot(CCWindow mainWindow){
 			var bounds = mainWindow.WindowSizeInPixels;
 
-			ballSprite = new CCSprite ("blueball");
-			ballSprite.Scale = bounds.Width/5000;
-			ballSprite.PositionX = bounds.Width/5000;
-			ballSprite.PositionY = bounds.Height/5000;
-			mainLayer.AddChild (ballSprite);
+			pivotSprite = new CCSprite ("pivot");
+			pivotSprite.Scale = 0.0002f*bounds.Width;
+			pivotSprite.PositionX = 0.5f*bounds.Width;
+			pivotSprite.PositionY =0.5f*bounds.Height;
+			mainLayer.AddChild (pivotSprite);
 
 
 		}
-		*/
+
+
 
 	}
 
---
+++ IsJustABall/PCL/Notes/HookedParticle.cs
@@ -0,0 +1,48 @@
+﻿/*
+float dRadius_X = ballSprite.PositionX- pivotSprite.PositionX;
+float dRadius_Y = ballSprite.PositionY- pivotSprite.PositionY;
+Math.Pow(value, power)
+float Radius = Math.Pow(dRadius_X,2) + Math.Pow(dRadius_Y,2);
+Radius = Math.Pow(Radius,0.5d);
+
+float ballSpeed =Math.Pow(ballXVelocity,2) + Math.Pow(ballYVelocity,2);
+ballSpeed = Math.pow(ballSpeed,0.5d);
+
+float CosThetaZero = dRadius_X/Radius;
+float SinThetaZero = dRadius_Y/Radius;
+
+float CosAlpha =(dRadius_X*ballXVelocity + dRadius_Y*ballYVelocity)/(Radius*ballSpeed);
+
+if( Math.Pow(CosAlpha,2) < 0.05){
+	CosAlpha = (0.05)^(0.5);
+	}
+float SinAlpha = (1 - Math.Pow(CosAlpha,2);
+	SinAlpha = Math.Pow(SinAlpha,0.5f);
+
+double theta=0;
+float Multiplier =1.0;
+float wZero = ballSpeed/Radius;
+
+float ballSpeedFinal = Radius*Multiplier*wZero*SinAlpha;
+
+if(SinThetaZero <0){
+	CosThetaZero = 2*Math.PI - CosThetaZero;
+	}
+//Above is calculated Once on Touch
+
+
+// below is the iteration over time
+theta = (double)(CosThetaZero + Multiplier*wZero*SinAlpha*frameTimeInSeconds);
+
+ballSprite.PositionX = Radius*Math.Cos(theta) + pivotSprite.PositionX;
+ballSprite.PositionY = Radius*Math.Sin(theta) + pivotSprite.PositionY;
+//ConsolePrint
+Console.WriteLine( "\n Math.Sin({0} deg) == {1:E16}\n" + "Math.Cos({0} deg) == {2:E16}", degrees, Math.Sin(theta), Math.Cos(theta) );
+
+ballXVelocity = ballSpeedFinal*Math.Sin(theta);
+ballYVelocity = ballSpeedFinal*Math.Cos(theta);
+
+
+
+
+*/
